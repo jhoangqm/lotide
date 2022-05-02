19a8b1f78121da0de4b1753680c62a5965829824
jhoangqm <jimmyhoangmtl@gmail.com>
2022-05-02 18:01:29 +0000

    Assert assignment

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = (actual, expected) =>{
+  if (actual === expected) {
+    return console.log(`🔥 Assertion success:  ${actual} === ${expected}`);
+  } else {
+    return console.log(`🛑 Assertion failed:  ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual('Lighthouse Labs', "Bootcamp");
+assertEqual(1, 1);
