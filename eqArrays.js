9e7fcac0423a0ce560eec50aa23399cf220be74f
jhoangqm <jimmyhoangmtl@gmail.com>
2022-05-03 20:59:54 +0000

    Refactoring the loop section

--- eqArrays.js
+++ eqArrays.js
@@ -8,22 +8,17 @@ const assertEqual = (actual, expected) =>{
 
 // Create a function which takes in two arrays and returns true or false based on a perfect match
 const eqArrays = function(arr1, arr2) {
-// define a boolean value of a variable start with false
-  let equal = false;
-  // for loop to increment to check elements in each array via an index
-  for (let i = 0; i < arr1.length; i++) {
-    // if both array indexes are equal the variable will be true
-    if (arr1[i] === arr2[i]) {
-      equal = true;
-      // otherwise the variable will be false
-    } else {
-      equal = false;
+    // for loop to increment to check elements in each array via an index
+    for (let i = 0; i < arr1.length; i++) {
+      // if both array indexes are equal the variable will be true
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
     }
-  }
-  // return results to see if both are perfect match
-  return equal;
+    // return results to see if both are perfect match
+    return true;
+  };
 
-};
 console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
 console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
 
