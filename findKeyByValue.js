a67da24711044bb9a89f906b6c5bc9b930493a66
jhoangqm <jimmyhoangmtl@gmail.com>
2022-05-04 19:14:55 +0000

    Added assertEqual function to our initial findKeyByValue.js commit

---
+++ findKeyByValue.js
@@ -0,0 +1,7 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    return console.log(`🔥 Assertion success:  ${actual} === ${expected}`);
+  } else {
+    return console.log(`🛑 Assertion failed:  ${actual} !== ${expected}`);
+  }
+};
