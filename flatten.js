648d4afba1fd462460cb597ea700d68fdfb8af57
jhoangqm <jimmyhoangmtl@gmail.com>
2022-05-04 00:27:24 +0000

    Updated flatten.js with a function

--- flatten.js
+++ flatten.js
@@ -1,6 +1,17 @@
 //flatten is a function which does something like this. 
 // Given an array with other arrays inside, it can flatten it into a single-level array.
 
+// Create flatten function which will contain the nested array
+const flatten = function(arr){
+  // Define a variable to an array and trying out built in function reduce previous to current
+  let newArr = arr.reduce((previous, current) => {
+    // return the output of built in function concatenating two arrays
+    return previous.concat(current);
+  },[]);
+  // return the newly built array
+  return newArr;
+}
+  
 const eqArrays = function(arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
@@ -19,4 +30,4 @@ const assertArraysEqual = function(arr1, arr2) {
 };
 
 assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => false
-console.log(flatten([1, 2, [3, 4], 5, [6]]) // => [1, 2, 3, 4, 5, 6]
+console.log(flatten([1, 2, [3, 4], 5, [6], 7, [8], 9])); // => [1, 2, 3, 4, 5, 6]
