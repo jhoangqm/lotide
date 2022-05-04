7f4faa3652139c1b6fc765fb3a9fe2ae87c7e339
jhoangqm <jimmyhoangmtl@gmail.com>
2022-05-04 20:19:39 +0000

    Initial findKeyByValue.js commit

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -5,3 +5,21 @@ const assertEqual = (actual, expected) => {
     return console.log(`🛑 Assertion failed:  ${actual} !== ${expected}`);
   }
 };
+
+const findKeyByValue = function (object, value) {
+  for (const key in object) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+};
+
+// Test cases
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
