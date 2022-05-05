282427b7c839445c9daf9738178b7d57e5987f4e
jhoangqm <jimmyhoangmtl@gmail.com>
2022-05-05 18:10:46 +0000

    removed a comment

--- takeUntil.js
+++ takeUntil.js
@@ -34,7 +34,6 @@ const takeUntil = function (array, callback) {
   }
 };
 
-// // expected Input
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, (x) => x < 0);
 console.log(results1);
