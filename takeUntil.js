ad368ff6d46946883e4ea2ce886d1444861300bc
jhoangqm <jimmyhoangmtl@gmail.com>
2022-05-05 18:09:23 +0000

    Initial commit of takeUntil.js without logic in the newly created function

---
+++ takeUntil.js
@@ -0,0 +1,52 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    return console.log(`🔥 Assertion success:  ${arr1} === ${arr2}`);
+  } else {
+    return console.log(`🛑 Assertion failed:  ${arr1} !== ${arr2}`);
+  }
+};
+
+// The function will return slice of the array with elements taken from
+// the beginning
+
+const takeUntil = function (array, callback) {
+  let results = [];
+  return results;
+};
+
+// // expected Input
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, (x) => x < 0);
+console.log(results1);
+
+console.log("---");
+
+const data2 = [
+  "I've",
+  "been",
+  "to",
+  "Hollywood",
+  ",",
+  "I've",
+  "been",
+  "to",
+  "Redwood",
+];
+const results2 = takeUntil(data2, (x) => x === ",");
+console.log(results2);
+
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
