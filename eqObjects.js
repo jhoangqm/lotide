a93ad4aef206152a2cddb2935a6df7b48c6bbf0a
jhoangqm <jimmyhoangmtl@gmail.com>
2022-05-08 17:51:51 +0000

    Refactored the eqObjects code to use recursion

--- eqObjects.js
+++ eqObjects.js
@@ -4,6 +4,32 @@ const eqArrays = require("./eqArrays");
 const eqObjects = function (object1, object2) {
   const keys1 = Object.keys(object1);
   const keys2 = Object.keys(object2);
+  if (
+    keys1.length !== keys2.length ||
+    Array.isArray(object1) ||
+    Array.isArray(object2)
+  )
+    return false;
+  for (const key of keys1) {
+    // added recursion if it's an instanceof Object
+    if (object2[key] instanceof Object || object1[key] instanceof Object) {
+      return eqObjects(object1[key], object2[key]);
+      // otherwise if its not equal to an object return false
+    } else if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// Old eqObjects just in case
+/*
+const assertEqual = require("./assertEqual");
+const eqArrays = require("./eqArrays");
+
+const eqObjects = function (object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
   if (keys1.length !== keys2.length) return false;
   for (const key of keys1) {
     if (Array.isArray(object1[key])) {
@@ -24,5 +50,7 @@ const eqObjects = function (object1, object2) {
   }
   return true;
 };
+
+*/
 
 module.exports = eqObjects;
