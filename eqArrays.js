1d1d9777c17a72f56c8b01a954a115bdcf13c92f
jhoangqm <jimmyhoangmtl@gmail.com>
2022-05-08 18:53:05 +0000

    refactored using recursion to check arrays within arrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,9 +1,16 @@
 const assertEqual = require("./assertEqual");
 
 const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      if (!eqArrays(arr1[i], arr2[i])) {
+        return false;
+      } else if (arr1[1] !== arr2[2]) {
+        return false;
+      }
     }
   }
   return true;
