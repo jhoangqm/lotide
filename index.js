d4beab6416389a65f15c5a2a006de12cecd23023
jhoangqm <jimmyhoangmtl@gmail.com>
2022-05-09 17:43:40 +0000

    Refactored using the genius shorthand from ES6

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const head = require("./head");
 const tail = require("./tail");
 const middle = require("./middle");
+const countLetters = require("./countLetters");
+const countOnly = require("./countOnly");
+const eqArrays = require("./eqArrays");
+const eqObjects = require("./eqObjects");
+const findKey = require("./findKey");
+const findKeyByValue = require("./findKeyByValue");
+const flatten = require("./flatten");
+const letterPositions = require("./letterPositions");
+const map = require("./map");
+const takeUntil = require("./takeUntil");
+const without = require("./without");
 
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle,
+  head,
+  tail,
+  middle,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  letterPositions,
+  map,
+  takeUntil,
+  without,
 };
