f0f40e27d27fed3febd4069db7d0ec2711133e3f
jhoangqm <jimmyhoangmtl@gmail.com>
2022-05-09 14:15:41 +0000

    minor adjustment on without.js file the function wasn't returning the results

--- without.js
+++ without.js
@@ -3,6 +3,7 @@ const assertArraysEqual = require("./assertArraysEqual");
 
 const without = function (arr1, arr2) {
   const newArr = arr1.filter((results) => !arr2.includes(results));
+  return newArr;
 };
 
 module.exports = without;
