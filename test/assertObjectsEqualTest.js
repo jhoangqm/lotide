01d9058ce313730aa9b18d7cbe087d159cd8ddfc
jhoangqm <jimmyhoangmtl@gmail.com>
2022-05-09 21:13:41 +0000

    Added missing export module for assertObjectsEqualTest.js

--- test/assertObjectsEqualTest.js
+++ test/assertObjectsEqualTest.js
@@ -2,4 +2,6 @@ const assertObjectsEqual = require("../assertObjectsEqual");
 
 assertObjectsEqual({ a: 1, b: 2 }, { a: 1, b: 2 });
 assertObjectsEqual({ a: 1, b: 5 }, { a: 1, b: 5 });
+
+module.exports = assertObjectsEqual;
 //
