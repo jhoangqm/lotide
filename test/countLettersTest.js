ef44f4029a5b9c86584a6e4a0000315f8bf355c5
jhoangqm <jimmyhoangmtl@gmail.com>
2022-05-09 16:46:20 +0000

    refactoring file to test using mocha and chai

---
+++ test/countLettersTest.js
@@ -0,0 +1,14 @@
+const { assert } = require("chai");
+const countLetters = require("../countLetters");
+
+const results1 = countLetters("how are you today");
+const results2 = countLetters("lighthouse in the house");
+
+describe("#countLetters", () => {
+  it("should return 1 for results1['t']", () => {
+    assert.strictEqual(results1["t"], 1);
+  });
+  it("should return 4 for results2['h']", () => {
+    assert.strictEqual(results2["h"], 4);
+  });
+});
