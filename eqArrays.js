c049226d3b110e57119578b53bcb9a0f2927ac64
jhoangqm <jimmyhoangmtl@gmail.com>
2022-05-09 17:01:18 +0000

    Fixed small bug that was return true for everything

--- eqArrays.js
+++ eqArrays.js
@@ -1,19 +1,20 @@
-const assertEqual = require("./assertEqual");
-
 const eqArrays = function (arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
+
   for (let i = 0; i < arr1.length; i++) {
     if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
       if (!eqArrays(arr1[i], arr2[i])) {
         return false;
-      } else if (arr1[1] !== arr2[2]) {
-        return false;
       }
+    } else if (arr1[i] !== arr2[i]) {
+      return false;
     }
   }
   return true;
 };
+
+console.log(eqArrays([1, 2, 3], [3, 2, 1]));
 
 module.exports = eqArrays;
